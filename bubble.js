5577612ed9a057c9c17378616cf998c8ae0f98fd
Nucoco <yuuta11511997@gmail.com>
2021-05-01 21:37:56 +0900

    master

---
+++ bubble.js
@@ -0,0 +1,162 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let particleArray = [];
+let adjustX = 30;
+let adjustY = 0;
+// the interval between a Particle and the next Particle in LetterArea
+const interval = 25;
+ctx.lineWidth = 3;
+
+//handle mouse
+const mouse = { x: null, y: null, radius: 250 };
+
+window.addEventListener("mousemove", function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  //   console.log(mouse.x, mouse.y);
+});
+
+ctx.fillStyle = "white";
+ctx.font = "30px Verdana";
+ctx.fillText("A", 0, 30);
+const textCoordinates = ctx.getImageData(0, 0, 100, 100);
+
+class Particle {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = 3;
+    this.baseX = this.x;
+    this.baseY = this.y;
+    this.density = Math.random() * 8 + 1; //:speed  *see 78, 79 line
+    this.distance;
+  }
+  draw() {
+    ctx.fillStyle = "rgba(255,255,255,0.8)";
+    ctx.strokeStyle = "rgba(34,147,214,1)";
+    ctx.beginPath();
+
+    if (this.distance < mouse.radius - 50) {
+      this.size = 13;
+      ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+      ctx.stroke();
+      ctx.closePath();
+      //highlights of a bubble
+      ctx.beginPath();
+      ctx.arc(this.x - 3, this.y - 3, this.size / 2, 0, Math.PI * 2);
+      ctx.arc(this.x + 1, this.y + 1, this.size / 3, 0, Math.PI * 2);
+    } else if (this.distance <= mouse.radius) {
+      this.size = 10;
+      ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+      ctx.stroke();
+      ctx.closePath();
+      ctx.beginPath();
+      ctx.arc(this.x - 2, this.y - 2, this.size / 3, 0, Math.PI * 2);
+    } else {
+      this.size = 8;
+      ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+      ctx.stroke();
+      ctx.closePath();
+      ctx.beginPath();
+      ctx.arc(this.x - 1, this.y - 1, this.size / 3, 0, Math.PI * 2);
+    }
+
+    ctx.closePath();
+    ctx.fill();
+  }
+  update() {
+    let dx = mouse.x - this.x;
+    let dy = mouse.y - this.y;
+    let distance = Math.sqrt(dx * dx + dy * dy);
+    this.distance = distance;
+    let forceDirectionX = dx / distance;
+    let forceDirectionY = dy / distance;
+    let maxDistance = mouse.radius;
+    let force = (maxDistance - distance) / maxDistance;
+    let directionX = forceDirectionX * force * this.density;
+    let directionY = forceDirectionY * force * this.density;
+    //マウスの周りを避けて動くアニメーション
+    if (distance < mouse.radius) {
+      //   this.size = 30;
+      this.x -= directionX;
+      this.y -= directionY;
+      //元の場所に戻るアニメーション
+    } else {
+      //   this.size = 3;
+      if (this.x !== this.baseX) {
+        let dx = this.x - this.baseX;
+        this.x -= dx / 10;
+      }
+
+      if (this.y !== this.baseY) {
+        let dy = this.y - this.baseY;
+        this.y -= dy / 10;
+      }
+    }
+  }
+}
+
+// console.log(textCoordinates);
+// console.log(textCoordinates.data);
+function init() {
+  particleArray = [];
+  for (let y = 0, y2 = textCoordinates.height; y < y2; y++) {
+    for (let x = 0, x2 = textCoordinates.width; x < x2; x++) {
+      if (
+        textCoordinates.data[y * 4 * textCoordinates.width + x * 4 + 3] > 128
+      ) {
+        let positionX = x + adjustX;
+        let positionY = y + adjustY;
+        particleArray.push(
+          new Particle(positionX * interval, positionY * interval)
+        );
+      }
+    }
+  }
+}
+init();
+// console.log(particleArray);
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < particleArray.length; i++) {
+    particleArray[i].draw();
+    particleArray[i].update();
+  }
+  // connect();
+  requestAnimationFrame(animate);
+}
+animate();
+
+function connect() {
+  let opacityValue = 1;
+  for (let a = 0; a < particleArray.length; a++) {
+    for (let b = a; b < particleArray.length; b++) {
+      // let dx = mouse.x - this.x;
+      // let dy = mouse.y - this.y;
+      // let distance = Math.sqrt(dx*dx +dy*dy);
+      let dx = particleArray[a].x - particleArray[b].x;
+      let dy = particleArray[a].y - particleArray[b].y;
+      let distance = Math.sqrt(dx * dx + dy * dy);
+      //56は横に2個,縦に1個ずれたParticleとの距離 Math.sqrt(interval * interval + 2 * interval * 2 * interval)
+      if (distance < 56) {
+        /*ここから */
+
+        //距離に応じて薄く（細く）なっていく
+        //102 は 56*2 で、最小の不透明度を0.5にしてる
+        opacityValue = 1 - distance / 102;
+
+        ctx.strokeStyle = "rgba(255, 255, 255," + opacityValue + ")";
+        /*ここまでを if文 の外側に出せるけど、 なんか動作重くなる*/
+
+        ctx.lineWidth = 2;
+        ctx.beginPath();
+        ctx.moveTo(particleArray[a].x, particleArray[a].y);
+        ctx.lineTo(particleArray[b].x, particleArray[b].y);
+        ctx.stroke();
+      }
+    }
+  }
+}
